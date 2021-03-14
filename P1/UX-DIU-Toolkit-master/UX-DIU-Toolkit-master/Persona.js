0e95060c5bea03a3ebcffac7bf55f69f6f64e461
angelsc21 <42170840+angelsc21@users.noreply.github.com>
2021-03-14 12:40:43 +0100

    Add files via upload

--- P1/UX-DIU-Toolkit-master/UX-DIU-Toolkit-master/Persona.js
+++ P1/UX-DIU-Toolkit-master/UX-DIU-Toolkit-master/Persona.js
@@ -33,36 +33,36 @@ angular.module("angular", [])
                 
                 
 				Id: 0,
-				Name: "Antonio",
+				Name: "Pedro",
 				Photo: "man.png",
-				Quote: "Hola que tal",
-				Age: 40,
-				Occupation: "Comercial en empresa de automocion",
-				Family: "Con pareja desde hace 5 año",
-				Location: "Granada (Huetor Vega)",
-				Character: "Le gusta disfrutar del tiempo",
+				Quote: "La vida es buena por sólo dos cosas, descubrir y enseñar las matemáticas. – Simeon Poisson",
+				Age: 55,
+				Occupation: "Profesor de matemáticas.",
+				Family: "20 años casado con su mujer Carmen y con 2 hijos (Javier y Alejandra).",
+				Location: "Jaén (Cazorla).",
+				Character: "Es muy risueño, calmado y optimista.",
 				PersonalityTraits: [
-					{ Name: "Hey/reservado Vs  Extrov/activo ", Value: 4 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
-					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 3 },
-					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 5 }
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 4 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 2 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 2 }
 				], 
-				Goals: ["Disfrutar del tiempo libre, viajar", "Cambiar a un trabajo más estable"],
-				Frustrations: ["le gusta la tecnología, pero siempre 'llama a un amigo' para resolver problemas", "Le gustaría tener más tiempo libre y leer más"],
-				Bio: "Es de Modril y vino a Granada para estudiar administración de empresas, pero no ha tenido grandes oportunidades de trabajo. LLeva 2 años contratado en la empresa actual. Aqui ha hecho buenos amigo en el trabajo y normalmente ser reunen para fiestas y a veces organizan viajes",
+				Goals: ["Disfrutar el tiempo libre con la familia.", "Descubrir nuevas culturas.", "Aprovechar ahora que ha adquirido cierta sabiduría con los años y tiene una vida más tranquila.", "Escribir un libro sobre la relación entre las matemáticas y el modus-operandi que crea en las personas"],
+				Frustrations: ["Sabe lo justo de tecnología, sólo para temas académicos.", "Le gustaría hacer planes diferentes a los habituales.", "No haber podido conocer tanto mundo cómo le gustaría.", "Por cuestiones económicas no permitirse darle a sus hijos una mejor vida con experiencias más enriquecedoras"],
+				Bio: "Es de Cazorla, estudió el grado de matemáticas en la Universidad de Jaén y tras prepararse las oposiciones durante 4 años le dieron una plaza en un pueblo Cádiz dónde conoció a su futura mujer. Posteriormente, consiguión reasignarse a su pueblo natal y formó allí su familia. Desde entonces, no han hecho vida mucho más alla de la provincia de Jaén y les gustaría conocer mundo.",
 				Tech: [
 					{ Name: "TIC/Internet", Value: 2 },
 					{ Name: "Movil", Value: 2 },
-					{ Name: "RRSS", Value: 3 },
+					{ Name: "RRSS", Value: 1 },
 					{ Name: "Software", Value: 2 }
 					
 				], 
-                Contextos: "LLeva un tiempo preocupado y quiere desconectar Le gustaría dar una sorpresa a sus amigos para las vacaciones",  
+                Contextos: "Su familia llevaba unos años bastante mal económicamente y ahora que se han recuperado le gustaría compensarles con un viaje.",  
 				PreferredChannels: [
-					{ Name: "Publicidad Tradicional", Value: 5 },
-					{ Name: "Online & Social Media", Value: 3 },
-					{ Name: "Recomendaciones & sugerencias", Value: 2 },
-					{ Name: "Persona confianza (amigos, boca a boca)", Value: 1 }
+					{ Name: "Publicidad Tradicional", Value: 3 },
+					{ Name: "Online & Social Media", Value: 1 },
+					{ Name: "Recomendaciones & sugerencias", Value: 3 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 4 }
 				]
 			},
 			{	
@@ -74,36 +74,36 @@ angular.module("angular", [])
                 
                 
 				Id: 1,
-				Name: "Monica Suarez",
+				Name: "Irene Pérez",
 				Photo: "woman.png",
-				Quote: "A quotation that captures the essence of this person's personality",
-				Age: 17,
-				Occupation: "Searching for a cure for the Empress",
-				Family: "No parents, only family are the people who raised him.",
-				Location: "The Grassy Plains of Fantasia",
-				Character: "Strong, reliable and fearless.",
+				Quote: " La medicina es el arte de disputar los hombres a la muerte de hoy, para cedérselos en mejor estado, un poco más tarde. (Noel Clarasó)",
+				Age: 22,
+				Occupation: "Estudiante",
+				Family: "Su padre Juan está en los 54 años, su madre María en los 50 años y su hermana Marta tiene 16",
+				Location: "Badajoz",
+				Character: "Es una chica decidida, amistosa y muy fiel a sus seres queridos",
 				PersonalityTraits: [
-					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 3 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 3 },
-					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
-					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 2 }
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 4 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 4 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 3 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 3 }
 				], 
-				Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
-				Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
-				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
+				Goals: ["En un futuro quiere tener su propia clínica privada.", "Le gustaría acabar la carrera de medicina en 6 años.", "Quiere tener 2 hijos.", "Le encantaría hacer uno o varios viajes con sus compañeras de piso."],
+				Frustrations: ["Le perturba mucho la idea de suspender un examen e ir a la extraordinaria.", "No puede ahorrar tanto cómo quisiera ya que tiene que pagar muchas cosas cómo el piso, comida, vida en la ciudad ...", "Le encantaría empezar a viajar más ahora ya que con su familia solo ha ido espontáneamente de vacaciones de verano a la playa.", "Está un poco cansada de su entorno y le gustaría conocer cómo es la vida fuera del país."],
+				Bio: "Irene es una chica de Bajadoz que tras pasar toda su infancia y adolescencia allí, decidió ir a estudiar a otra ciudad y empezó la carrera de medicina en Madrid. Siempre ha sido una chica muy centrada en los estudios, con fuertes vínculos emocionales a sus personas más cercanas y un sentimiento interno de conocer más tipos de vida de los que estaba acostumbrada a ver. Es muy ambiciosa con su futuro y está decidida a lograr todo aquello que se fije cómo meta. Ahora mismo está en un punto de su vida en el que tiene una gran amistad con sus compañeras de piso y le gustaría hacer un viaje con ellas para así aprovechar y descubrir mundo junto a una gran compañía.",
 				Tech: [
-					{ Name: "TIC/Internet", Value: 5 },
-					{ Name: "Mobile", Value: 3 },
-					{ Name: "RRSS", Value: 3 },
-					{ Name: "Software", Value: 5 }
+					{ Name: "TIC/Internet", Value: 3 },
+					{ Name: "Mobile", Value: 4 },
+					{ Name: "RRSS", Value: 4 },
+					{ Name: "Software", Value: 2 }
 					
 				], 
-                Contextos:   "The goals this user hopes to achieve." ,
+                Contextos:   "Está en un punto de su vida en el que está abierta a conocer nuevas experiencias a diario. Ahora que ha conseguido un fuerte vínculo con sus compañeras de piso le gustaría poder hacer un viaje con ellas para que además de tener un recuerdo inolvidable con ellas pueda conocer mundo ya que antes no había podido." ,
 				PreferredChannels: [
-					{ Name: "Publicidad Tradicional (Ads)", Value: 5 },
-					{ Name: "Online & Social Media", Value: 2 },
-					{ Name: "Recomendaciones & sugerencias", Value: 2 },
-					{ Name: "Persona confianza (amigos, boca a boca)", Value: 2 }
+					{ Name: "Publicidad Tradicional (Ads)", Value: 2 },
+					{ Name: "Online & Social Media", Value: 5 },
+					{ Name: "Recomendaciones & sugerencias", Value: 3 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 4 }
 				]
 			}
 		];
